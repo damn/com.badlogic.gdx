23e17cbc201fb0eae28ce415bd323a571988a36e
Michael Sappler <michaelsappler@gmail.com>
2026-05-30 11:09:54 +0530

    Remove freetype

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
-(defproject gdx "-SNAPSHOT"
+(defproject com.badlogic.gdx "1.14.0"
   :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [
-                 [com.badlogicgames.gdx/gdx                   "1.14.0"]
-                 [com.badlogicgames.gdx/gdx-freetype          "1.14.0"]
-                 [com.badlogicgames.gdx/gdx-freetype-platform "1.14.0" :classifier "natives-desktop"]
+                 [com.badlogicgames.gdx/gdx "1.14.0"]
                  [org.clojure/clojure "1.12.0"]
                  ]
   :source-paths ["src"]
@@ -11,6 +9,6 @@
   :resource-paths ["resources/"]
   :plugins [[lein-hiera "2.0.0"]
             [lein-codox "0.10.8"]]
-  :codox {:source-uri "https://github.com/damn/gdx/blob/main/{filepath}#L{line}"
+  :codox {:source-uri "https://github.com/damn/com.badlogic.gdx/blob/main/{filepath}#L{line}"
           :metadata {:doc/format :markdown}}
   :global-vars {*warn-on-reflection* true})
