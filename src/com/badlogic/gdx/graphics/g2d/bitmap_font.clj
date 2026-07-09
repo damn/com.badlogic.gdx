18eae07dc28ab5321596beafdb19f9954529b657
Michael Sappler <michaelsappler@gmail.com>
2026-07-10 01:38:57 +0530

    Add sprite-batch and texture-region

--- src/com/badlogic/gdx/graphics/g2d/bitmap_font.clj
+++ src/com/badlogic/gdx/graphics/g2d/bitmap_font.clj
@@ -1,8 +1,16 @@
 (ns com.badlogic.gdx.graphics.g2d.bitmap-font
-  (:import (com.badlogic.gdx.graphics.g2d Batch BitmapFont)))
+  (:import (com.badlogic.gdx.graphics.g2d Batch
+                                          BitmapFont)))
 
 (defn draw [font ^Batch batch text x y target-width halign wrap?]
-  (.draw ^BitmapFont font batch text (float x) (float y) (float target-width) halign wrap?))
+  (.draw ^BitmapFont font
+         batch
+         text
+         (float x)
+         (float y)
+         (float target-width)
+         halign
+         wrap?))
 
 (defn getData [font]
   (.getData ^BitmapFont font))
